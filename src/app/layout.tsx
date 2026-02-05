aba5b545efc842efa568cfc3cb7f0181b46ce153
Carlos Eduardo Teixeira Zulian <71992281+CaduZulian@users.noreply.github.com>
2026-02-04 21:53:36 -0300

    feat: update libs and fix bugs in font and animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -194,6 +194,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang={getSystemLanguage().name}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
       <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} />
 
       <SpeedInsights />
